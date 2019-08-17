c1ad653b1ccd464d7ec84d5439d1b4c6b575f03d
pengchaoqun <pengchaoqun@fzxs.com.cn>
2019-08-17 22:16:07 +0800

    修改了ico图标

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SmallDaily'`, () => {
+  it(`should have as title 'ASmallStep'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SmallDaily');
+    expect(app.title).toEqual('ASmallStep');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SmallDaily!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ASmallStep!');
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server_url: 'http://localhost:80/api/api'
-  // server_url: 'http://192.168.0.102:8011/api/api'
+  //server_url: 'http://localhost:80/api/api'
+  server_url: 'http://192.168.0.102:80/api/api'
   //server_url: 'http://localhost:59113/api/'
 };
